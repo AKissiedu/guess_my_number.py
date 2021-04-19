356b87dce31e6e3b85f014f620c11d473a479a25
AKissiedu <82220061+AKissiedu@users.noreply.github.com>
2021-04-19 20:33:05 +0000

    Add files via upload

---
+++ guess_my_number_starter.py
@@ -0,0 +1,32 @@
+"""
+File: guessmynumber.py
+----------------------
+This program checks if a user's guess of a number matches that guessed by the computer
+"""
+
+import random
+# use code below  to generate a random integer between 30 and 50 for example
+# print(random.randint(30, 50))
+
+# ********************************** YOUR CODE GOES BELOW HERE *********************************************************
+# The variable secret_number randomly stores a randomly generated number to be guessed
+# The variable guessed_right is a boolean that is set to True once the number is guessed right
+
+secret_number = random.randint(1, 99)
+guessed_right = False
+
+# The while loop then executes until guessed_right is set to True
+
+while not guessed_right:
+    guessed_number = int(input('Please guess the number:'))
+
+# The if/elif/else statements check whether the guessed number is the same, higher or lower than the secret number.
+    if guessed_number == secret_number:
+        print('Congrats! The number was:', secret_number)
+        guessed_right = True
+    elif guessed_number > secret_number:
+        print('Your guess is too high, try again')
+
+    else:
+        print('Your guess is too low, try again')
+
