720711128bc196be07ae649d6a743fca60b1437a
AKissiedu <82220061+AKissiedu@users.noreply.github.com>
2021-04-19 21:56:11 +0000

    Update guess_my_number_starter.py

--- guess_my_number_starter.py
+++ guess_my_number_starter.py
@@ -12,21 +12,24 @@ import random
 # The variable secret_number randomly stores a randomly generated number to be guessed
 # The variable guessed_right is a boolean that is set to True once the number is guessed right
 
+
 secret_number = random.randint(1, 99)
 guessed_right = False
 
+
+guessed_number = int(input('Please enter a guess:'))
 # The while loop then executes until guessed_right is set to True
 
 while not guessed_right:
-    guessed_number = int(input('Please guess the number:'))
-
-# The if/elif/else statements check whether the guessed number is the same, higher or lower than the secret number.
+    # The if/elif/else statements check whether the guessed number is the same, higher or lower than the secret number.
     if guessed_number == secret_number:
         print('Congrats! The number was:', secret_number)
         guessed_right = True
     elif guessed_number > secret_number:
         print('Your guess is too high, try again')
-
+        guessed_number = int(input('Please enter a new guess:'))
     else:
         print('Your guess is too low, try again')
+        guessed_number = int(input('Please enter a new guess:'))
+print('The number was {}\n'. format(secret_number))
 
